c994289ac784cdd4ab533b6f96880ab76e945be7
esterAtz <EsterAtz2011>
2018-11-26 13:14:39 +0200

    lab2 inital commit

--- labs/lab2/2.1- FlightDelay-load-data-Exercise/main/java/common/DelayRec.java
+++ labs/lab2/2.1- FlightDelay-load-data-Exercise/main/java/common/DelayRec.java
@@ -9,26 +9,17 @@ import com.gigaspaces.metadata.index.SpaceIndexType;
  * Created by ester on 07/01/2018.
  */
 public class DelayRec implements java.io.Serializable{
+    // Corrospond to CSV of following structure
+    //Year	Month	DayofMonth	DayOfWeek	DepTime	CRSDepTime	ArrTime	CRSArrTime	UniqueCarrier	FlightNum	TailNum	ActualElapsedTime	CRSElapsedTime	AirTime	ArrDelay	DepDelay	Origin	Dest	Distance	TaxiIn	TaxiOut	Cancelled	CancellationCode	Diverted	CarrierDelay	WeatherDelay	NASDelay	SecurityDelay	LateAircraftDelay
+
+    // ToDo Build a class represnting part of the fields in this record
+    // Must include dayOfWeek, crsDepTime, depDelay, month, dayOfMonth, year
+
     //Flight number + date
     String id;
 
     String flightNumber;
 
-    Integer year;
-
-    Integer month;
-
-    Integer dayOfMonth;
-
-    Integer dayOfWeek;
-
-    Integer crsDepTime;
-
-    Integer depDelay;
-
-    String origin;
-
-    double distance;
 
     @SpaceId
     public String getId() {
@@ -47,6 +38,12 @@ public class DelayRec implements java.io.Serializable{
     public void setFlightNumber(String flightNumber) {
         this.flightNumber = flightNumber;
     }
+
+    // ToDo add more fields, Decide on indexes
+    // ToDo Add default constuctor
+    // ToDo Add constructor with expected fields
+    // ToDo verify you understand ID and routing see: https://docs.gigaspaces.com/xap/14.0/dev-java/space-object-id-operations.html
+    // And https://docs.gigaspaces.com/xap/14.0/dev-java/pojo-attribute-annotations.html
 
     public Integer getYear() {
         return year;
--- labs/lab2/README.md
+++ labs/lab2/README.md
@@ -1,1 +1,30 @@
-lab2
+#Lab2
+
+# Flight Delay Example
+## Getting Started
+
+
+Data for the demo
+Link:  http://stat-computing.org/dataexpo/2009/the-data.html
+
+Instruction:
+1. Start insightedge Manager 
+    insightedge host run-agent --auto
+2. Go to web-ui - (http://localhost:8099)
+3. Strat 2 GSC using web-ui (can be done as part of cmd line as well)
+4. Download from the above Link, 2007 csv files.
+   Have a look at the file decide which fields  To keep in space, 
+   Define DelayRec class accordingly.
+   Space Routing field can be set to Flight Number
+   Space Id a field containing flight number and time.  
+5. Build the space jar 
+6. Go to rest manager(http://localhost:8090/v2/) and Deploy space jar (2,0 or start more gsc)
+(Can be done using cmd line or in web-ui as well)
+5. Download from the above Link, 2007 csv files.
+7. Go to zeplin (http://localhost:9090) , import lab notebook (json in resorces)
+8. Fix in dependencies section the location of the jar, so it will point to your local jar.
+9. Fix location of 2007 in Saving Flight delay RDD to the Grid after reading from csv paragraph and in simulate new data paragraph
+10. Run the notebook.(see grid console to see delays were reported)
+11. Go to web-ui to see if the data is saved correctly.
+
+
