873e1d733f4d096a3f87ff6e43e0200cbb9bf842
esteratz <ester.atzmon@gigaspaces.com>
2021-10-03 08:40:33 +0300

    add test for localview in feeder, update version to 16.0.0

--- Day4/17_TieredStorage/feeder/src/main/java/com/gs/Feeder.java
+++ Day4/17_TieredStorage/feeder/src/main/java/com/gs/Feeder.java
@@ -1,8 +1,10 @@
 package com.gs;
 
+import com.j_spaces.core.client.SQLQuery;
 import org.openspaces.core.GigaSpace;
 import org.openspaces.core.GigaSpaceConfigurer;
 import org.openspaces.core.space.SpaceProxyConfigurer;
+import org.openspaces.core.space.cache.LocalViewSpaceConfigurer;
 
 public class Feeder {
     GigaSpace gs;
@@ -32,10 +34,27 @@ public class Feeder {
         System.out.println("read of Data1 took:" + (System.currentTimeMillis() - start2) + " res:" + res4.length);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         GigaSpace gs1 = new GigaSpaceConfigurer(new SpaceProxyConfigurer("test1")).gigaSpace();
+        LocalViewSpaceConfigurer localViewConfigurer = new LocalViewSpaceConfigurer(new SpaceProxyConfigurer("test1"))
+                .batchSize(1000)
+                .batchTimeout(100)
+                .maxDisconnectionDuration(1000*60*60)
+                .addProperty("space-config.engine.memory_usage.high_watermark_percentage", "90")
+                .addProperty("space-config.engine.memory_usage.write_only_block_percentage", "88")
+                .addProperty("space-config.engine.memory_usage.write_only_check_percentage", "86")
+                .addProperty("space-config.engine.memory_usage.retry_count", "5")
+                .addProperty("space-config.engine.memory_usage.explicit", "false")
+                .addProperty("space-config.engine.memory_usage.retry_yield_time", "50")
+                .addViewQuery(new SQLQuery(com.gs.Data1.class, ""));
+// Create local view:
+        GigaSpace localView = new GigaSpaceConfigurer(localViewConfigurer).gigaSpace();
+
         Feeder feeder = new Feeder(gs1);
         feeder.feed(1000);
         feeder.read(1000);
+        Thread.sleep(1000);
+        Data1 obj = localView.read(new Data1());
+        System.out.println("got object:" + obj);
     }
 }
