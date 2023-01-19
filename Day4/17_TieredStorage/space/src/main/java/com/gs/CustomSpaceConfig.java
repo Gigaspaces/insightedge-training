ad7c3b5daa6354e3508f2e37428cc2311e9118ea
esteratz <ester.atzmon@gigaspaces.com>
2023-01-19 16:19:20 +0200

    fix version 16.1.1

--- Day4/17_TieredStorage/space/src/main/java/com/gs/CustomSpaceConfig.java
+++ Day4/17_TieredStorage/space/src/main/java/com/gs/CustomSpaceConfig.java
@@ -22,8 +22,12 @@ import org.openspaces.core.cluster.ClusterInfo;
 import org.openspaces.core.cluster.ClusterInfoContext;
 import org.openspaces.core.config.annotation.EmbeddedSpaceBeansConfig;
 import org.openspaces.core.space.EmbeddedSpaceFactoryBean;
+import org.openspaces.core.space.TieredStorageCachePolicy;
+//import org.openspaces.core.space.TieredStorageCachePolicy;
 
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 public class CustomSpaceConfig extends EmbeddedSpaceBeansConfig {
@@ -36,19 +40,33 @@ public class CustomSpaceConfig extends EmbeddedSpaceBeansConfig {
     protected void configure(EmbeddedSpaceFactoryBean factoryBean) {
 		super.configure(factoryBean);
 		Properties properties = new Properties();
-        properties.setProperty("space-config.engine.cache_policy", "1"); //change to 4 for 16.2
+        //properties.setProperty("space-config.engine.cache_policy", "4"); //for 16.2+
 		properties.setProperty("space-config.engine.query.result.size.limit", "400");
 		properties.setProperty("space-config.engine.query.result.size.limit.memory.check.batch.size", "250");
 		properties.setProperty("cluster-config.groups.group.repl-policy.sync-replication.target-consume-timeout", "500");
+		properties.setProperty("space-config.leader-election.zookeeper.connection-timeout", "30000");
+		properties.setProperty("space-config.leader-election.zookeeper.session-timeout", "90000");
 		factoryBean.setProperties(properties);
 
 		TieredStorageConfig tieredStorageConfig = new TieredStorageConfig();
-		tieredStorageConfig.addTable(new TieredStorageTableConfig().setName("Doc1").setCriteria("type='4'"));
-		tieredStorageConfig.addTable(new TieredStorageTableConfig().setName("Doc2").setTransient(true));
-		tieredStorageConfig.addTable(new TieredStorageTableConfig().setName("Doc4").setCriteria("all"));
-		tieredStorageConfig.addTable(new TieredStorageTableConfig().setName("Doc5").setTimeColumn("expire").setPeriod( Duration.ofHours(24)));
-		//factoryBean.setCachePolicy(new TieredStorageCachePolicy(tieredStorageConfig)); --> 16.2
+//16.1.1
+		//Criteria per table can be defined in space configuration or when registering the type
+		Map<String, TieredStorageTableConfig> tables = new HashMap<>();
+		tables.put("MyTestClass", new TieredStorageTableConfig().setName("MyTestClass").setCriteria("all"));
+		tables.put("Doc1", new TieredStorageTableConfig().setName("Doc1").setCriteria("type='4'"));
+		tables.put("Doc2", new TieredStorageTableConfig().setName("Doc2").setTransient(true));
+		tables.put("Doc4", new TieredStorageTableConfig().setName("Doc4").setCriteria("all"));
+		tables.put("Doc5", new TieredStorageTableConfig().setName("Doc5").setTimeColumn("expire").setPeriod( Duration.ofHours(24)));
+
+		tieredStorageConfig.setTables(tables);
 		factoryBean.setTieredStorageConfig(tieredStorageConfig);
+
+//16.2
+	//	tieredStorageConfig.addTable(new TieredStorageTableConfig().setName("Doc1").setCriteria("type='4'"));
+	//	tieredStorageConfig.addTable(new TieredStorageTableConfig().setName("Doc2").setTransient(true));
+	//	tieredStorageConfig.addTable(new TieredStorageTableConfig().setName("Doc4").setCriteria("all"));
+	//	tieredStorageConfig.addTable(new TieredStorageTableConfig().setName("Doc5").setTimeColumn("expire").setPeriod( Duration.ofHours(1)));
+	//	factoryBean.setCachePolicy(new TieredStorageCachePolicy(tieredStorageConfig));
 
 
 	}
